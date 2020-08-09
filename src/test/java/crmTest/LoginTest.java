a6ce42f930c6330ac35bcc4a5293c005629da561
Arjun Dahal <arjun_dhl@yahoo.com>
2020-08-08 21:40:43 -0500

    first commit

--- src/test/java/crmTest/LoginTest.java
+++ src/test/java/crmTest/LoginTest.java
@@ -1,14 +1,16 @@
 package crmTest;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-
+//
 	WebDriver wd;
 
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void mainFile() {
 		LoginTest obj = new LoginTest();
 		obj.launchBrowser();
 		obj.login();
